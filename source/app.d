7a138346fac65cc80ff2a6a39f55cf973f042517
Vuk Uskokovic <49611825+vukuskokovic@users.noreply.github.com>
2019-12-29 19:04:27 +0100

    Update app.d

--- source/app.d
+++ source/app.d
@@ -1,6 +1,33 @@
-import std.stdio;
+module gpioAPI;
+import std.file;
+import std.format;
+import std.conv;
 
-void main()
+string sysPath = "/sys/class/gpio/";
+
+enum GPIOPinState{
+    HIGH,
+    LOW
+}
+
+enum GPIOPinDirection
 {
-	writeln("Edit source/app.d to start your project.");
+    IN,
+    OUT
+}
+
+void exportPin(int pin){
+    append(format!"%s%s"(sysPath, "export"), to!string(pin));
+}
+
+void unexportPin(int pin){
+    append(format!"%s%s"(sysPath, "unexport"), to!string(pin));
+}
+
+void setPinDirection(int pin, GPIOPinDirection direction){
+    append(format!"%sgpio%d/direction"(sysPath, pin), direction == GPIOPinDirection.IN? "in" : "out");
+}
+
+void setPinState(int pin, GPIOPinState state){
+    append(format!"%sgpio%d/value"(sysPath, pin), state == GPIOPinState.HIGH? "1" : "0");
 }
